6ebc572060379db310addf59100f68a2bfcea0ef
Ryan <jxdalang2015@gmail.com>
2015-12-04 00:11:25 +0800

    new solution for q22

--- leetcode/Q22_generate_parentheses/generate_parentheses/Solution.java
+++ leetcode/Q22_generate_parentheses/generate_parentheses/Solution.java
@@ -22,21 +22,21 @@ For example, given n = 3, a solution set is:
  */
 public class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<String>();
-        if (n == 0) {
-            result.add("");
-        } else {
-            for (int i = n - 1; i >= 0; i--) {
-                List<String> insertSub = generateParenthesis(i);
-                List<String> tailSub = generateParenthesis(n - 1 - i);
-                for (String insert : insertSub) {
-                    for (String tail : tailSub) {
-                        result.add("(" + insert + ")" + tail);
-                    }
-                }
+        List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
 
-            }
+    private void backtrack(List<String> list, String str, int open, int close, int max){
+
+        if(str.length() == max*2){
+            list.add(str);
+            return;
         }
-        return result;
+
+        if(open < max)
+            backtrack(list, str+"(", open+1, close, max);
+        if(close < open)
+            backtrack(list, str+")", open, close+1, max);
     }
 }
