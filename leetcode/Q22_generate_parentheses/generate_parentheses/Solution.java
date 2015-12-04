46a7887fdc24e6b5df6c26aee37decc045bec4b2
Ryan <jxdalang2015@gmail.com>
2015-12-04 10:37:12 +0800

    add comment to q22

--- leetcode/Q22_generate_parentheses/generate_parentheses/Solution.java
+++ leetcode/Q22_generate_parentheses/generate_parentheses/Solution.java
@@ -27,16 +27,32 @@ public class Solution {
         return list;
     }
 
-    private void backtrack(List<String> list, String str, int open, int close, int max) {
+    private void backtrack(List<String> list, String str, int open, int close, int n) {
+        // open和close分别记录当前传入的str中'('和')'的个数
+        // n是需要使用的括号对个数
 
-        if (str.length() == max * 2) {
+        // 如果str中已经有n对括号了，说明str是一个有效的解，将其加入list中
+        if (str.length() == n * 2) {
             list.add(str);
             return;
         }
 
-        if (open < max)
-            backtrack(list, str + "(", open + 1, close, max);
+        // 保证每个左括号总有一个对应的右括号，即都是有效的解
+        if (open < n)
+            backtrack(list, str + '(', open + 1, close, n);
         if (close < open)
-            backtrack(list, str + ")", open, close + 1, max);
+            backtrack(list, str + ')', open, close + 1, n);
+
+        return;
+    }
+
+    // test generateParenthesis method
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.generateParenthesis(0));
+        System.out.println(s.generateParenthesis(1));
+        System.out.println(s.generateParenthesis(2));
+        System.out.println(s.generateParenthesis(3));
+        System.out.println(s.generateParenthesis(4));
     }
 }
