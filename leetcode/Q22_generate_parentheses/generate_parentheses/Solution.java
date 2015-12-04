b423e73f6aff72c999d8af27c00bf6866fb7fb94
Ryan <jxdalang2015@gmail.com>
2015-12-04 12:49:10 +0800

    finish q22

--- leetcode/Q22_generate_parentheses/generate_parentheses/Solution.java
+++ leetcode/Q22_generate_parentheses/generate_parentheses/Solution.java
@@ -23,6 +23,7 @@ For example, given n = 3, a solution set is:
 public class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> list = new LinkedList<String>();
+        // 调用回溯方法求解，从初始str=空字符串开始寻找解
         backtrack(list, "", 0, 0, n);
         return list;
     }
@@ -31,16 +32,20 @@ public class Solution {
         // open和close分别记录当前传入的str中'('和')'的个数
         // n是需要使用的括号对个数
 
-        // 如果str中已经有n对括号了，说明str是一个有效的解，将其加入list中
+        // 如果str中已经有n对括号了，说明str已经是一个有效的解，将其加入list中
         if (str.length() == n * 2) {
             list.add(str);
             return;
         }
 
-        // 保证每个左括号总有一个对应的右括号，即都是有效的解
+        // 这一解法的实质是按照回溯的方法找出所有可能的解，并且总是优先尝试放入'('
         if (open < n)
+            // 递归调用
             backtrack(list, str + '(', open + 1, close, n);
+        
+        // 保证每个左、右括号个数相等
         if (close < open)
+            // 递归调用
             backtrack(list, str + ')', open, close + 1, n);
 
         return;
