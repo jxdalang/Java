c663c04016a8515864648552d64dbb4037e00831
Ryan Zhou <jxdalang2015@gmail.com>
2016-08-29 15:30:13 +0800

    add 2 solutions in codility

---
+++ codility/lesson/prefix_sums/PassingCars.java
@@ -0,0 +1,16 @@
+/**
+* @package prefix_sums
+* @author ZS@ZJU
+* @version V1.0
+*/
+package prefix_sums;
+
+/**
+* @className PassingCars
+* @description TODO
+* @author ZS@ZJU
+* @date Aug 29, 2016 3:07:37 PM
+*/
+public class PassingCars {
+
+}
